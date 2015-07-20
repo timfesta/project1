09649cbefe5e6c0c701aef8f54d8af335d6b4ec5
Tim Festa <sbfestival@Tims-MacBook-Air-2.local>
2015-07-20 13:06:28 -0700

    base

---
+++ stock.js
@@ -0,0 +1,37 @@
+$(function () {
+	console.log('ciabatta')
+
+//----- SEED DATA -----///
+	var lines = [
+		{text: "citi"},
+		{text: "citi"},
+		{text: "citi"}
+	]
+
+//----- TEMPLATE - inputs into html-----//
+ $line = _.template( $("#lineTemplate").html() )
+
+
+//---- WHATEVER YOU INPUT => ADD TO LIST ----//
+ _.each(lines, function(yourInput) {
+      console.log(yourInput)
+      $('#lines').append($line(yourInput))
+    })
+
+//-----EVENT LISTENER-------//
+ $('#new-line').submit(function(e){
+    e.preventDefault();
+    console.log("im submitting a form")
+
+//-----GRABBING THE VALUE FROM THE INPUT LINE----//
+    var line = {
+      text: $('#line-text').val()
+    }
+    //- grabs the data from the input line runs it through template and adds to list---//
+      $('#lines').prepend($line(line))
+   
+
+  })
+})
+
+
