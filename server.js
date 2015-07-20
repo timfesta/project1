c813ca35a41efeed7255d07baff6f38774399464
Tim Festa <sbfestival@Tims-MacBook-Air-2.local>
2015-07-20 15:22:47 -0700

    move to server

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
-mongoose.connect(
-  process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
-  'mongodb://localhost/YOUR_LOCAL_DATABASE_NAME' // plug in the db name you've been using
-);
+// require express framework and additional modules
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser');
+
+// tell app to use bodyParser middleware
+app.use(bodyParser.urlencoded({extended: true}));
+
+// set up root route to respond with 'hello world'
+app.get('/', function (req, res) {
+  res.send('ciabatta');
+});
 
 
-app.listen(process.env.PORT || 3000);
+app.get('/api/stocks', function (req, res) {
+	var stocks = [
+		{text: "citi"},
+		{text: "citi"},
+		{text: "citi"}
+	]
+	res.json(stocks)
+})
+
+// mongoose.connect(
+//   process.env.MONGOLAB_URI ||
+//   process.env.MONGOHQ_URL ||
+//   'mongodb://localhost/YOUR_LOCAL_DATABASE_NAME' // plug in the db name you've been using
+// );
+
+
+// listen on port 3000
+app.listen(3000, function () {
+  console.log('server started on localhost:3000');
+});
--- stock.js
+++ stock.js
@@ -1,37 +1,45 @@
-$(function () {
-	console.log('ciabatta')
+$(function() {
+	console.log("Ello Gubna")
 
 //----- SEED DATA -----///
-	var lines = [
-		{text: "citi"},
-		{text: "citi"},
-		{text: "citi"}
-	]
+	// var stocks = [
+	// 	{text: "citi"},
+	// 	{text: "citi"},
+	// 	{text: "citi"}
+	// ]
 
 //----- TEMPLATE - inputs into html-----//
- $line = _.template( $("#lineTemplate").html() )
+ $stock = _.template( $("#stockTemplate").html() )
+
+  $.get(baseUrl + '/api/stocks', function(data) {
+    var stocks = data 
 
 
 //---- WHATEVER YOU INPUT => ADD TO LIST ----//
- _.each(lines, function(yourInput) {
+ _.each(stocks, function(yourInput) {
       console.log(yourInput)
-      $('#lines').append($line(yourInput))
+      $('#stocks').append($stock(yourInput))
     })
 
 //-----EVENT LISTENER-------//
- $('#new-stock').submit(function(e){
-    e.preventDefault();
+ $('#new-stock').submit(function(event){
+    event.preventDefault();
     console.log("im submitting a form")
 
 //-----GRABBING THE VALUE FROM THE INPUT LINE----//
-    var line = {
-      text: $('#line-text').val()
+    var stock = {
+      text: $('#stock-text').val()
     }
-    //- grabs the data from the input line runs it through template and adds to list---//
-      $('#lines').prepend($line(line))
-   
+    //- grabs the data from the input line runs it through template and adds to list
+      $('#stocks').prepend($stock(stock))
+
+
+    $.post('/api/stocks', stock, function(data) {
+      console.log(data)
+      $('#stocks').prepend($stock(data))
+    })
 
   })
 })
-
+})
 
