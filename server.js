87ad0e4c6bf47cc32fdbd14de994e8bbe0cc2b25
Tim Festa <sbfestival@Tims-MacBook-Air-2.local>
2015-07-22 12:59:27 -0700

    login

--- server.js
+++ server.js
@@ -2,11 +2,14 @@
 // require frameworks and additional modules--//
 var express = require('express'),
     app = express(),
-    _ = require('underscore'),
-    cors = require('cors'),
     bodyParser = require('body-parser'),
     mongoose = require("mongoose"),
-    session = require('express-session'); // is this a dup OF LINE 25    
+    User = require('./models/user'),
+    session = require('express-session');
+    _ = require('underscore'),
+    // cors = require('cors'),
+     
+       
 
 
 //---- Connection to the DB------------------//
@@ -19,12 +22,40 @@ var Stock = require('./security')
 
 
 //---tell app to use bodyParser middleware and cors---//
-app.use(cors());
+// app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+  saveUninitialized: true,
+  resave: true,
+  secret: 'SuperSecretCookie',
+  cookie: { maxAge: 60000 }
+}));
 
+ app.use(express.static(__dirname));
 
-app.use(express.static(__dirname));
+// middleware to manage sessions
+app.use('/', function (req, res, next) {
+  // saves userId in session for logged-in user
+  req.login = function (user) {
+    req.session.userId = user.id;
+  };
 
+  // finds user currently logged in based on `session.userId`
+  req.currentUser = function (callback) {
+    User.findOne({_id: req.session.userId}, function (err, user) {
+      req.user = user;
+      callback(null, user);
+    });
+  };
+
+  // destroy `session.userId` to log out user
+  req.logout = function () {
+    req.session.userId = null;
+    req.user = null;
+  };
+
+  next();
+});
 
 //------------- Route to home page-----------//
 app.get('/', function (req, res) {
@@ -32,6 +63,58 @@ app.get('/', function (req, res) {
   res.sendFile(index);
 });
 
+
+//---------ROUTE TO SIGNUPs----------------//
+// signup route with placeholder response
+app.get('/signup', function (req, res) {
+  res.send('coming soon');
+});
+
+// user submits the signup form
+app.post('/users', function (req, res) {
+
+  // grab user data from params (req.body)
+  var newUser = req.body.user;
+
+  // create new user with secure password
+  User.createSecure(newUser.email, newUser.password, function (err, user) {
+    res.send(user);
+  });
+});
+
+// login route (renders login view)
+app.get('/login', function (req, res) {
+  res.sendFile(__dirname + '/public/views/login.html');
+});
+
+
+// user submits the login form
+app.post('/login', function (req, res) {
+
+  // grab user data from params (req.body)
+  var userData = req.body.user;
+
+  // call authenticate function to check if password user entered is correct
+  User.authenticate(userData.email, userData.password, function (err, user) {
+    // saves user id to session
+    req.login(user);
+
+    // redirect to user profile
+    res.redirect('/profile');
+  });
+});
+
+// user profile page
+app.get('/profile', function (req, res) {
+  // finds user currently logged in
+  req.currentUser(function (err, user) {
+    res.send('Welcome ' + user.email);
+  });
+});
+
+
+
+//---------ROUTE TO STOCKS PAGE-----------//
 app.get('/stocks', function (req, res) {
   var stocks = __dirname + "/stocks.html"; 
 //----- SEED DATA -----///
@@ -41,8 +124,6 @@ app.get('/stocks', function (req, res) {
 		{text: "BOA"}
 	]
   res.sendFile(stocks);
-
-
 })
 
 
